8718aa66879e35232ee9adccb5523c3c3ed27e86
Tom Palmer <remlapmot@hotmail.com>
2025-04-10 10:21:30 +0100

    Update install-binary.R

--- install-binary.R
+++ install-binary.R
@@ -1,38 +1,26 @@
+install.packages(
+  "pak",
+  repos = sprintf(
+    "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
+    .Platform$pkgType,
+    R.Version()$os,
+    R.Version()$arch
+  )
+)
+
 if (Sys.info()["sysname"] != "Linux") {
   # Windows and macOS
-  install.packages(
-    "pak",
-    repos = sprintf(
-      "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
-      .Platform$pkgType,
-      R.Version()$os,
-      R.Version()$arch
-    )
-  )
   pak::repo_add(CRAN = "https://cloud.r-project.org")
   pak::repo_add(universe = "https://mrcieu.r-universe.dev")
   pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
-} else if (Sys.info()["machine"] != "arm64") {
+} else if (Sys.info()["machine"] == "x86_64") {
   # Linux x86_64
-  install.packages(
-    "pak",
-    repos = sprintf(
-      "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
-      .Platform$pkgType,
-      R.Version()$os,
-      R.Version()$arch
-    )
-  )
   pak::repo_add(CRAN = "https://mrcieu.r-universe.dev/bin/linux/noble/4.4/")
   pak::repo_add(universe = "https://p3m.dev/cran/__linux__/noble/latest")
   pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
-} else if (Sys.info()["machine"] != "arm64") {
-  # Linux arm64
-  options(
-    repos = c(
-      CRAN = "https://cloud.r-project.org",
-      universe = "https://mrcieu.r-universe.dev"
-    )
-  )
-  install.packages("TwoSampleMR", dependencies = TRUE)
+} else if (Sys.info()["machine"] == "aarch64") {
+  # Linux aarch64
+  pak::repo_add(CRAN = "https://cloud.r-project.org")
+  pak::repo_add(universe = "https://mrcieu.r-universe.dev")
+  pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
 }
