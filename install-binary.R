e517da404391d75c8e68db40f19527cd34ce0163
Tom Palmer <remlapmot@hotmail.com>
2025-04-10 10:11:05 +0100

    Update install-binary.R

--- install-binary.R
+++ install-binary.R
@@ -1,20 +1,28 @@
-install.packages(
-  "pak",
-  repos = sprintf(
-    "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
-    .Platform$pkgType,
-    R.Version()$os,
-    R.Version()$arch
-  )
-)
-
 if (Sys.info()["sysname"] != "Linux") {
   # Windows and macOS
+  install.packages(
+    "pak",
+    repos = sprintf(
+      "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
+      .Platform$pkgType,
+      R.Version()$os,
+      R.Version()$arch
+    )
+  )
   pak::repo_add(CRAN = "https://cloud.r-project.org")
   pak::repo_add(universe = "https://mrcieu.r-universe.dev")
   pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
 } else if (Sys.info()["machine"] != "arm64") {
   # Linux x86_64
+  install.packages(
+    "pak",
+    repos = sprintf(
+      "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
+      .Platform$pkgType,
+      R.Version()$os,
+      R.Version()$arch
+    )
+  )
   pak::repo_add(CRAN = "https://mrcieu.r-universe.dev/bin/linux/noble/4.4/")
   pak::repo_add(universe = "https://p3m.dev/cran/__linux__/noble/latest")
   pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
