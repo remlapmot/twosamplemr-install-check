6f3697718661cc7755cedd76144517c5e4975728
Tom Palmer <remlapmot@hotmail.com>
2025-04-10 10:05:08 +0100

    Update install-binary.R

--- install-binary.R
+++ install-binary.R
@@ -9,17 +9,22 @@ install.packages(
 )
 
 if (Sys.info()["sysname"] != "Linux") {
-    # Windows and macOS
-    pak::repo_add(CRAN = "https://cloud.r-project.org")
-    pak::repo_add(universe = "https://mrcieu.r-universe.dev")
+  # Windows and macOS
+  pak::repo_add(CRAN = "https://cloud.r-project.org")
+  pak::repo_add(universe = "https://mrcieu.r-universe.dev")
+  pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
 } else if (Sys.info()["machine"] != "arm64") {
-    # Linux x86_64
-    pak::repo_add(CRAN = "https://mrcieu.r-universe.dev/bin/linux/noble/4.4/")
-    pak::repo_add(universe = "https://p3m.dev/cran/__linux__/noble/latest")
+  # Linux x86_64
+  pak::repo_add(CRAN = "https://mrcieu.r-universe.dev/bin/linux/noble/4.4/")
+  pak::repo_add(universe = "https://p3m.dev/cran/__linux__/noble/latest")
+  pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
 } else if (Sys.info()["machine"] != "arm64") {
-    # Linux arm64
-    pak::repo_add(CRAN = "https://cloud.r-project.org")
-    pak::repo_add(universe = "https://mrcieu.r-universe.dev")
+  # Linux arm64
+  options(
+    repos = c(
+      CRAN = "https://cloud.r-project.org",
+      universe = "https://mrcieu.r-universe.dev"
+    )
+  )
+  install.packages("TwoSampleMR", dependencies = TRUE)
 }
-
-pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
