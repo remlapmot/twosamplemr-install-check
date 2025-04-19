125a666255d93f1738a6d9e9f9d5547beb30652c
Tom Palmer <remlapmot@hotmail.com>
2025-04-19 07:01:54 +0100

    Add session info

--- install-binary.R
+++ install-binary.R
@@ -12,15 +12,19 @@ if (Sys.info()["sysname"] != "Linux") {
   # Windows and macOS
   pak::repo_add(CRAN = "https://cloud.r-project.org")
   pak::repo_add(universe = "https://mrcieu.r-universe.dev")
-  pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
+  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE, upgrade = TRUE)
 } else if (Sys.info()["machine"] == "x86_64") {
   # Linux x86_64
   pak::repo_add(CRAN = "https://mrcieu.r-universe.dev/bin/linux/noble/4.4/")
   pak::repo_add(universe = "https://p3m.dev/cran/__linux__/noble/latest")
-  pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
+  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE, upgrade = TRUE)
 } else if (Sys.info()["machine"] == "aarch64") {
   # Linux aarch64
   pak::repo_add(CRAN = "https://cloud.r-project.org")
   pak::repo_add(universe = "https://mrcieu.r-universe.dev")
-  pak::pkg_install("TwoSampleMR", dependencies = TRUE, upgrade = TRUE)
+  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE, upgrade = TRUE)
 }
+
+library(TwoSampleMR)
+
+sessioninfo::session_info()
