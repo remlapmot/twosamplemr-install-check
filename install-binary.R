2109f9305be3b2959719c6addda777ac6ee91dc3
Tom Palmer <remlapmot@hotmail.com>
2025-04-26 12:45:29 +0100

    Attempted fix

--- install-binary.R
+++ install-binary.R
@@ -12,17 +12,21 @@ if (Sys.info()["sysname"] != "Linux") {
   # Windows and macOS
   pak::repo_add(CRAN = "https://cloud.r-project.org")
   pak::repo_add(universe = "https://mrcieu.r-universe.dev")
-  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE, upgrade = TRUE)
+  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE)
 } else if (Sys.info()["machine"] == "x86_64") {
   # Linux x86_64
-  pak::repo_add(CRAN = "https://mrcieu.r-universe.dev/bin/linux/noble/4.4/")
+  if (getRversion() >= '4.5.0') {
+    pak::repo_add(CRAN = "https://mrcieu.r-universe.dev/bin/linux/noble/4.5/")
+  } else if (getRversion() < '4.5.0') {
+    pak::repo_add(CRAN = "https://mrcieu.r-universe.dev/")
+  }
   pak::repo_add(universe = "https://p3m.dev/cran/__linux__/noble/latest")
-  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE, upgrade = TRUE)
+  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE)
 } else if (Sys.info()["machine"] == "aarch64") {
   # Linux aarch64
   pak::repo_add(CRAN = "https://cloud.r-project.org")
   pak::repo_add(universe = "https://mrcieu.r-universe.dev")
-  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE, upgrade = TRUE)
+  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE)
 }
 
 library(TwoSampleMR)
