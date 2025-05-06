46aa95526771eff0c59ee12c145e09c48352b1ae
Tom Palmer <remlapmot@hotmail.com>
2025-05-06 09:20:01 +0100

    Fix for r-uni URLs

--- install-binary.R
+++ install-binary.R
@@ -12,6 +12,7 @@ if (Sys.info()["sysname"] != "Linux") {
   # Windows and macOS
   pak::repo_add(CRAN = "https://cloud.r-project.org")
   pak::repo_add(universe = "https://mrcieu.r-universe.dev")
+  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE)
 } else if (Sys.info()["machine"] == "x86_64") {
   # Linux x86_64
   print(getRversion())
@@ -24,23 +25,23 @@ if (Sys.info()["sysname"] != "Linux") {
   }
   pak::repo_add(CRAN = "https://p3m.dev/cran/__linux__/noble/latest")
   pak::repo_add(CRANbackup = "https://cloud.r-project.org")
+  
+  if (getRversion() >= '4.6.0') {
+    install.packages("TwoSampleMR", repos = c(universe = "https://mrcieu.r-universe.dev/bin/linux/noble-x86_64/4.6/", CRAN = "https://p3m.dev/cran/__linux__/noble/latest", CRANbackup = "https://cloud.r-project.org"), dependencies = TRUE)
+  } else if (getRversion() >= "4.3.2") {
+    pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE)
+  } else {
+    pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = NA)
+  }
 } else if (Sys.info()["machine"] == "aarch64") {
   # Linux aarch64
-  pak::repo_add(CRAN = "https://cloud.r-project.org")
   if (getRversion() >= "4.6.0") {
-    pak::repo_add(universe = "https://mrcieu.r-universe.dev/bin/linux/noble-aarch64/4.6/")
+    install.packages("TwoSampleMR", repos = c(universe = "https://mrcieu.r-universe.dev/bin/linux/noble-aarch64/4.6/", CRAN = 'https://cloud.r-project.org'), dependencies = TRUE)
   } else if (getRversion() == "4.5.0") {
-    pak::repo_add(universe = "https://mrcieu.r-universe.dev/bin/linux/noble-aarch64/4.5/")
-  } else {
-    pak::repo_add(universe = "https://mrcieu.r-universe.dev")
+    install.packages("TwoSampleMR", repos = c(universe = "https://mrcieu.r-universe.dev/bin/linux/noble-aarch64/4.5/", CRAN = "https://cloud.r-project.org"), dependencies = TRUE)
   }
 }
 
-if (getRversion() < "4.3.2") {
-  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = NA)
-} else {
-  pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE)
-}
 
 library(TwoSampleMR)
 
