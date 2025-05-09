6fb13a9d9ba5bd109d8aca48581dfad5f13afc16
Tom Palmer <remlapmot@hotmail.com>
2025-05-09 08:43:22 +0100

    Use pak devel
    To avoid DESCRIPTION is malformed error
    
    https://github.com/r-lib/pkgbuild/issues/208#issuecomment-2863033383

--- install-binary.R
+++ install-binary.R
@@ -1,7 +1,7 @@
 install.packages(
   "pak",
   repos = sprintf(
-    "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
+    "https://r-lib.github.io/p/pak/devel/%s/%s/%s",
     .Platform$pkgType,
     R.Version()$os,
     R.Version()$arch
