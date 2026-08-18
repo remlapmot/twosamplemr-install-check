82bd6978361650810e751af837e4633f4d72690a
Tom Palmer <remlapmot@hotmail.com>
2026-08-18 13:19:58 +0100

    Use cran.r-universe.dev as CRAN repo on Windows aarch64

--- install-binary.R
+++ install-binary.R
@@ -22,7 +22,12 @@ options(HTTPUserAgent = sprintf(
 # Windows and macOS
 if (Sys.info()["sysname"] != "Linux") {
   # Windows and macOS
-  pak::repo_add(CRAN = "https://cloud.r-project.org")
+  if (Sys.info()["sysname"] == "Windows" && R.version$arch == "aarch64") {
+    # Windows aarch64, CRAN does not build arm64 Windows binaries
+    pak::repo_add(CRAN = "https://cran.r-universe.dev")
+  } else {
+    pak::repo_add(CRAN = "https://cloud.r-project.org")
+  }
   pak::repo_add(universe = "https://mrcieu.r-universe.dev")
   pak::pkg_install(c("TwoSampleMR", "sessioninfo"), dependencies = TRUE)
 }
